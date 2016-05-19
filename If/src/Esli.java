3252baefc5a2a2948393ad4b343506f62fe578b4
ycheren_work <ycheren@n103381.te.mera.ru>
2016-05-19 13:56:28 +0300

    If

---
+++ If/src/Esli.java
@@ -0,0 +1,16 @@
+
+public class Esli {
+
+	public static void main(String args[]){
+		int a = 4;
+		int b = 3;
+		int c = 1;
+		
+		if (a > b && a > c )
+		System.out.println("а больше b , c");
+		else
+		System.out.println("а меньше кого то");
+		
+	}
+	
+}
