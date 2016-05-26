897e8e989dbfd4c864e48286366d2dce732dce59
ycheren_work <ycheren@n103381.te.mera.ru>
2016-05-26 15:26:59 +0300

    getters/setters

---
+++ Incapsulation/src/A1.java
@@ -0,0 +1,20 @@
+
+public class A1 {
+	
+	private int a = 5;
+	private int b = 23;
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	
+
+}
---
+++ Incapsulation/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		A1 obj = new A1();
+		System.out.println(obj.getA());
+obj.setA(77);
+System.out.println(obj.getA());
+	}
+
+}
